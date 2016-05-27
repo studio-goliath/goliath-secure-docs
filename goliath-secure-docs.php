3742b22e45628e13361eb6bc57c3a753df70b005
ArnaudBan <abanvillet@studio-goliath.com>
2016-05-27 10:27:32 +0200

    Create folder for secure doc on plugins activation

--- goliath-secure-docs.php
+++ goliath-secure-docs.php
@@ -16,3 +16,28 @@
 
 // Add custom post types
 require_once plugin_dir_path( __FILE__ ) . 'post-types/secure-document.php';
+
+
+/**
+ * Fonction call on plugin activation
+ *
+ *  - create secure document folder
+ */
+function goliath_secure_documents_activate() {
+
+    $secure_doc_folder_path = goliath_secure_documents_get_docs_folder();
+
+    mkdir( $secure_doc_folder_path, 0600 );
+
+}
+register_activation_hook( __FILE__, 'goliath_secure_documents_activate' );
+
+
+
+function goliath_secure_documents_get_docs_folder(){
+
+    $secure_doc_folder_path = dirname( ABSPATH );
+
+    return $secure_doc_folder_path . '/goliath-secure-documents';
+
+}
--- tests/test-secure-document.php
+++ tests/test-secure-document.php
@@ -10,5 +10,13 @@ class Goliath_Secure_Documents_Test extends WP_UnitTestCase {
 	{
 		$this->assertTrue( post_type_exists( 'secure-document' ) );
 	}
+
+	function test_goliath_secure_documents_folder_existe()
+	{
+
+		$secure_doc_folder_path = goliath_secure_documents_get_docs_folder();
+
+		$this->assertTrue( is_dir( $secure_doc_folder_path ) );
+	}
 }
 
